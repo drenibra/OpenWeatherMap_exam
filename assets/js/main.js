ad500c196d99d6b0081dd2b3a1279ac339fc0682
drenibra <drenibrahimi02@gmail.com>
2021-05-11 22:46:48 +0200

    five day forecast

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,10 @@
 $(document).ready(() => {
-    $('#searchForm').on('submit', (e) => {
+    $('#button').click((e) => {
         var cityName = $('#cityName').val();
-        getTime(cityName);
+        var forecastType = $('#forecastType').val();
+        getTime(cityName, forecastType);
         e.preventDefault();
-    });    
+    });
 });
 
 const apiKey = '7d780f1f4327bb82e2e03efc37e177e3';
@@ -13,7 +14,7 @@ let newDate = '';
 let temperature = '';
 let output = '';
 
-function getTime(place) {
+function getTime(place, forecastType) {
     const timeZoneApi = 'a55bf11d071c4c32b5dfd559020b2b0d';
     var url = `https://timezone.abstractapi.com/v1/current_time/?api_key=${timeZoneApi}&location=${place}`;
     axios.get(`${url}`)
@@ -23,7 +24,7 @@ function getTime(place) {
         newDate = new Date(date);
         newDate = newDate.toString().split(' ');
         newDate = `${newDate[0]} - ${newDate[1]} ${newDate[2]}`
-        getCity(place);
+        getCity(place, forecastType);
     })
     .catch((err) => {
         console.log((err));
@@ -31,18 +32,29 @@ function getTime(place) {
 }
 
 
-function getCity(city) {
+function getCity(city, forecastType) {
+    let forecast5Url = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${apiKey}`;
     var url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
-    axios.get(`${url}`)
-    .then((response) => {
-        renderWeather(response.data);
-    })
-    .catch((err) => {
-        console.log((err));
-    })
+    if (forecastType === 'current') {
+        axios.get(`${url}`)
+        .then((response) => {
+            renderCurrentWeather(response.data);
+        })
+        .catch((err) => {
+            console.log((err));
+        })
+    } else {
+        axios.get(`${forecast5Url}`)
+        .then((response) => {
+            renderFiveDayWeather(response.data);
+        })
+        .catch((err) => {
+            console.log((err));
+        })
+    }
 }
 
-function renderWeather(city) {
+function renderCurrentWeather(city) {
     output = '';
     temperature = parseInt(city.main.temp);
     let wind = parseInt(city.wind.speed * 3.6);
@@ -67,7 +79,8 @@ function renderWeather(city) {
                 </div>
                 <div class="col-lg-6 col-md-6 col-12 text-right">
                     <h2>${city.name}</h2>
-                    <span>${newDate}, ${time}</span><br>
+                    <h5>${time}</h5>
+                    <span>${newDate}</span></br>
                     <span>${capitalize(city.weather[0].description)}</span>
                 </div>
             </div>
@@ -75,6 +88,7 @@ function renderWeather(city) {
     `;
 
     $('#weatherOutput').html(output);
+
     var vm = new Vue({
         el: '#app',
         data: {
@@ -103,7 +117,67 @@ function renderWeather(city) {
     })
 }
 
+function renderFiveDayWeather(data) {
+    console.log(data);
+    data.list.forEach(function (item) {
+        let iconLink = `http://openweathermap.org/img/wn/${item.weather[0].icon}@4x.png`;
+        let time = (item.dt_txt.split(':')[0] + ':' + item.dt_txt.split(':')[1]).split(' ')[1];
+        let date = (item.dt_txt.split(':')[0] + ':' + item.dt_txt.split(':')[1]).split(' ')[0];
+        output += `
+        <div class="jumbotron item">
+            <div id="weather" class="row">
+                <div class="col-lg-6 col-md-6 col-sm-12 col-12 d-flex align-items-center">
+                    <div class="d-flex align-items-center">
+                        <img src="${iconLink}" alt="sunny_s_cloudy" style="width: 100px;">
+                        <h1 class="pl-3" id="temperatureValue">${parseInt(item.main.temp)}</h1>
+                    </div>
+                    <div class="d-flex align-items-center position-relative pl-1" style="top:-10px">
+                        <span class="pr-2">°C</span>
+                    </div>
+                    <div class="pl-2">
+                        <span>Humidity: ${item.main.humidity}%</span><br>
+                        <span>Wind: ${item.wind.speed}km/h</span>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-6 col-12 text-right">
+                    <h2>${data.city.name}</h2>
+                    <h5>${time}</h5>
+                    <span>${getWeatherDate(date)}</span></br>
+                    <span>${capitalize(item.weather[0].description)}</span>
+                </div>
+            </div>
+        </div>
+        `;
+        $('#weatherOutputCarousel').html(output);
+    })
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            }
+        }
+    });
+    $('.customNextBtn').click(function () {
+        $('.owl-carousel').trigger('next.owl.carousel');
+      }); 
+      $('.customPreviousBtn').click(function () {
+        $('.owl-carousel').trigger('prev.owl.carousel', [300]);
+      });
+}
+
 const capitalize = (s) => {
     if (typeof s !== 'string') return ''
     return s.charAt(0).toUpperCase() + s.slice(1)
+}
+
+function getWeatherDate(thisDate) {
+    thisDate = thisDate.split(' ')[0];
+    let dateYear = date.split('-')[0]
+    let dateMonth = date.split('-')[1]
+    let dateDay = date.split('-')[2]
+    let fullDate = dateMonth + '/' + dateDay + '/' + dateYear;
+    let dt = new Date(fullDate)
+    return dt.toString().split(' ')[0] + ' - ' + dt.toString().split(' ')[1] + ' ' +  dt.toString().split(' ')[2];
 }
